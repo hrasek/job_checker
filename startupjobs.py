9073a179b7b8ba6f54b7af4badf516d653f9957b
hrasek <hosekmotor@seznam.cz>
2023-10-23 10:57:40 +0200

    Update on scrapping the startupjobs web.

--- startupjobs.py
+++ startupjobs.py
@@ -1,11 +1,10 @@
-from bs4 import BeautifulSoup as BS
 import requests
 
 
-link = "https://www.startupjobs.cz/nabidky/vyvoj?uvazek=full-time&seniorita=junior&lokalita=ChIJEVE_wDqUEkcRsLEUZg-vAAQ&pouze-s-odmenou=1&odmena=50000-CZK-mesicne"
+link = "https://www.startupjobs.cz/api/offers?shift[]=full-time&seniority[]=junior&area[]=vyvoj&location[]=ChIJEVE_wDqUEkcRsLEUZg-vAAQ&salary[]=1&salaryRange[]=%7B%22value%22:%2250000%22,%22currency%22:%22CZK%22,%22measure%22:%22monthly%22%7D&page=3"
 r = requests.get(link)
-soup = BS(r.text, "html.parser")
-a = soup.find_all('a')
-for sublinks in a:
-    print(sublinks)
-    print("\n")
+a = r.json()
+result = a['resultSet']
+
+for item in result:
+    print(item['id'], item.keys())
