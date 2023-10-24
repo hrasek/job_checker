b91744e6ce107fb937f4199f8d8c501ea6bbdb74
hrasek <hosekmotor@seznam.cz>
2023-10-24 20:00:13 +0200

    List of jobs offers from startupjobs.cz succesfully created.

--- startupjobs.py
+++ startupjobs.py
@@ -1,10 +1,23 @@
 import requests
 
 
-link = "https://www.startupjobs.cz/api/offers?shift[]=full-time&seniority[]=junior&area[]=vyvoj&location[]=ChIJEVE_wDqUEkcRsLEUZg-vAAQ&salary[]=1&salaryRange[]=%7B%22value%22:%2250000%22,%22currency%22:%22CZK%22,%22measure%22:%22monthly%22%7D&page=3"
-r = requests.get(link)
-a = r.json()
-result = a['resultSet']
+link = "https://www.startupjobs.cz/api/offers?shift[]=full-time&seniority[]=junior&area[]=vyvoj&location[]=ChIJEVE_wDqUEkcRsLEUZg-vAAQ&salary[]=1&salaryRange[]=%7B%22value%22:%2250000%22,%22currency%22:%22CZK%22,%22measure%22:%22monthly%22%7D&page=1"
+i=1
+key_list = ['name', 'company', 'locations','shifts', 'seniorities', 'isRemote']
+jobs_list = []
+while True:
+    try:
+        link = link[0:-1] + str(i)
+        r = requests.get(link)
+        a = r.json()
+        result = a['resultSet']
+        for item in result:
+            row = []
+            for key in key_list:
+                row.append(item[key])
+            jobs_list.append(row)
+        i+=1
 
-for item in result:
-    print(item['id'], item.keys())
+    except:
+        break
+print(jobs_list)
